90509af6a881e6a95d4b55c88e9f83eb3f661c23
Lareb Noor <156116687+noor2607@users.noreply.github.com>
2024-03-26 13:49:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env Node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+const answer = await inquirer.prompt([{
+        name: "userguessedNumber",
+        type: "number",
+        message: "Plz Guess A Number Between 1-8.",
+    },
+]);
+if (answer.userguessedNumber === randomNumber) {
+    console.log("Hurrah!!! You Guessed The Right Number.");
+}
+else {
+    console.log("Sorry! Try Again.");
+}
